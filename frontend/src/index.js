b19e899c2960a60964d76e871ee12acf613aea62
salvo tomarchio <tomarchios00@gmail.com>
2025-05-13 01:07:09 +0200

    fix

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+require('dotenv').config();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
