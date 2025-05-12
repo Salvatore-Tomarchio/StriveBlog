744303cbc2679bdcccc57568428a52694540e455
salvo tomarchio <tomarchios00@gmail.com>
2025-05-13 01:40:38 +0200

    update frontend with new useEffect function

--- frontend/src/views/blog/Blog.jsx
+++ frontend/src/views/blog/Blog.jsx
@@ -37,13 +37,13 @@ const Blog = () => {
   const fetchAll = async () => {
     const token = localStorage.getItem("token");
     try {
-      const res = await fetch("http://localhost:3002/me", {
+      const res = await fetch(`${process.env.REACT_APP_API_URL}/me`, {
         headers: { Authorization: `Bearer ${token}` },
       });
       const data = await res.json();
       setAuthor(data);
 
-      const postsRes = await fetch(`http://localhost:3002/authors/${data._id}/blogPosts`);
+      const postsRes = await fetch(`${process.env.REACT_APP_API_URL}/authors/${data._id}/blogPosts`);
       const postsData = await postsRes.json();
       setPosts(postsData);
     } catch (err) {
