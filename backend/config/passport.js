6e94dd41e46ced34e3aac64c38db65567bb6cce9
salvo tomarchio <tomarchios00@gmail.com>
2025-05-13 01:25:23 +0200

    update  be

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -4,7 +4,7 @@ const passport = require("passport");
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/auth/google/callback"
+    callbackURL: process.env.GOOGLE_CALLBACK_URL
   },
   function(accessToken, refreshToken, profile, done) {
     done(null, profile);
